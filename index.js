52ed6fb84a3b800d7e863991f62ab38db2cb09ae
Jari Pennanen <ciantic@oksidi.com>
2021-07-25 01:00:58 +0300

    ...

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 import init, {
     sum_in_workers,
-    sum_in_workers2,
+    send_to_channel,
+    receive_from_channel,
     init_thread_workers,
 } from "./pkg/shared_wasm_experiments.js";
-const mod = await init();
-const threads = await init_thread_workers("./worker.js", 0, 3);
-console.log("threads", threads);
-console.log("SUM", await sum_in_workers());
-console.log(
-    "SUM",
-    await sum_in_workers2({
-        from: 0,
-        to: 50,
-    })
-);
+await init();
+await init_thread_workers("./worker.js", 8, 0);
+
+// TODO: WorkerPool halts until the receive_from_channel finishes.
+//
+// Most likely because the WorkerPool does not reserve the worker and queues on
+// individual workers instead of single queue
+
+// Rayon
+// ----------------------------------------------------------------------------
+
+sum_in_workers().then((s) => {
+    console.log("Sum numbers with rayon", s);
+});
+
+// Shared Channel
+// ----------------------------------------------------------------------------
+
+// Wait asynchronously in the worker for a result
+receive_from_channel().then((value) => {
+    console.log("Got from a channel!", value);
+});
+
+// Send a result to channel
+setTimeout(() => {
+    console.log("Send to channel!");
+    send_to_channel("Send me!");
+}, 1300);
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,8 @@
 mod pool;
 mod utils;
 
-use std::{
-    collections::HashMap,
-    sync::{Arc, RwLock},
-    time::Duration,
-};
-
 use crossbeam_channel::{unbounded, Receiver, Sender};
-use futures_channel::oneshot;
-use js_sys::{Promise, Reflect};
+use js_sys::Promise;
 use once_cell::sync::Lazy;
 use pool::run_in_worker;
 use rayon::iter::{IntoParallelIterator, ParallelIterator};
@@ -31,35 +24,15 @@ extern "C" {
 pub fn __dummy() {
     // TODO: Without this export, the wasm-pack didn't create imports.wbg and
     // failed?
-    unsafe {
-        log(&"Foo");
-    }
+    log(&"Foo");
 }
+
+// Rayon
+// ----------------------------------------------------------------------------
 
 #[wasm_bindgen]
 pub fn sum_in_workers() -> Promise {
-    run_in_worker(|| (0..100 as i32).into_par_iter().sum::<i32>())
-}
-
-// #[wasm_bindgen]
-// pub fn example_fun(v: JsValue) -> JsValue {}
-
-// Shared hash map
-// ----------------------------------------------------------------------------
-
-static CACHE: Lazy<Arc<RwLock<HashMap<i32, String>>>> =
-    Lazy::new(|| Arc::new(RwLock::new(HashMap::<i32, String>::new())));
-
-#[wasm_bindgen]
-pub fn add_to_map(key: i32, value: String) {
-    let mut c = CACHE.write().unwrap();
-    c.insert(key, value);
-}
-
-#[wasm_bindgen]
-pub fn get_from_map(key: i32) -> Option<String> {
-    let c = CACHE.read().unwrap();
-    c.get(&key).map(Clone::clone)
+    run_in_worker(|| (0..100000 as i32).into_par_iter().sum::<i32>())
 }
 
 // Shared Channel
@@ -73,11 +46,9 @@ pub fn send_to_channel(str: &str) {
 }
 
 #[wasm_bindgen]
-pub fn receive_from_channel() -> String {
-    // Using timeout is not possible:
-    // let value = CHANNEL.1.recv_timeout(Duration::from_millis(3000)).unwrap();
-
-    // We must receive infinitely
-    let value = CHANNEL.1.recv().unwrap();
-    return value;
+pub fn receive_from_channel() -> Promise {
+    run_in_worker(|| {
+        let value = CHANNEL.1.recv().unwrap();
+        value
+    })
 }
