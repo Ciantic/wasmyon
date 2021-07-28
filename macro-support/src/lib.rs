35d0bd5a001f842ddcd33d39ef1a3b034cfe3151
Jari Pennanen <ciantic@oksidi.com>
2021-07-28 17:04:18 +0300

    ...

--- macro-support/src/lib.rs
+++ macro-support/src/lib.rs
@@ -3,6 +3,10 @@ use proc_macro2::{Ident, Span, TokenStream as TokenStream2};
 use quote::{quote, ToTokens, TokenStreamExt};
 use syn::{parse_macro_input, parse_quote, token::RArrow, Attribute, ItemFn, ReturnType, Type};
 
+/// Runs the function in a WASM worker, returning a Promise
+///
+/// This also retains the original function, and creates a new function for the
+/// wasm named `__wasm_ORIGINAL_FUNCTION`.
 #[proc_macro_attribute]
 pub fn promise(attr: TokenStream, item: TokenStream) -> TokenStream {
     // println!("attr: \"{}\"", attr.to_string());
@@ -10,29 +14,29 @@ pub fn promise(attr: TokenStream, item: TokenStream) -> TokenStream {
     let original_fn: TokenStream2 = item.clone().into();
 
     // Parse the input function, get body and name
-    let mut input_fn: ItemFn = parse_macro_input!(item as ItemFn);
-    let input_fn_body: TokenStream2 = input_fn.block.to_token_stream();
-    let input_fn_name = format!("{}", input_fn.sig.ident);
+    let mut wasm_fn: ItemFn = parse_macro_input!(item as ItemFn);
+    let wasm_fn_body: TokenStream2 = wasm_fn.block.to_token_stream();
+    let wasm_fn_name = format!("{}", wasm_fn.sig.ident);
 
     // Set new name and body, return type is js_sys::Promise
-    input_fn.sig.ident = Ident::new(&("__wasm_".to_owned() + &input_fn_name), Span::call_site());
-    input_fn.block = Box::new(parse_quote!({
-        wasmyon::run_in_worker(move || #input_fn_body)
+    wasm_fn.sig.ident = Ident::new(&("__wasm_".to_owned() + &wasm_fn_name), Span::call_site());
+    wasm_fn.block = Box::new(parse_quote!({
+        wasmyon::run_in_worker(move || #wasm_fn_body)
     }));
-    input_fn.sig.output = ReturnType::Type(
+    wasm_fn.sig.output = ReturnType::Type(
         RArrow::default(),
         Box::new(Type::Verbatim(quote!(js_sys::Promise))),
     );
 
     // TODO: How do we change TypeScript output type to Promise<ORIGINALTYPE>?
     let attrs: TokenStream2 = parse_quote!(
-        #[wasm_bindgen(js_name = #input_fn_name)]
+        #[wasm_bindgen(js_name = #wasm_fn_name)]
     );
 
     TokenStream::from(quote! (
         #original_fn
 
         #attrs
-        #input_fn
+        #wasm_fn
     ))
 }
